e3d1d76ede96e3edc981ea252c56667a0a96121d
David Martinez Alcantara <martinezalcantara29@gmail.com>
2022-03-01 17:42:52 +0100

    clase practicas

--- Assets/Bezier/ControlPoint.cs
+++ Assets/Bezier/ControlPoint.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 public class ControlPoint : MonoBehaviour
@@ -19,47 +20,50 @@ public class ControlPoint : MonoBehaviour
 	
 	}
 }
-
 // Editor personalizado para Bezier
 [CustomEditor(typeof(ControlPoint))]
 public class ControlPointEditor : Editor
 {
+	// Guarda la ultima posicion del punto de control, si cambia ya se que tengo que actualizar la curva
+	private Vector3 _lastPosition;
+
 	// Cuando el Punto de Control esta seleccionado
 	public void OnSceneGUI()
 	{
-
 		ControlPoint cp = target as ControlPoint;
-		Bezier bezier = cp.bezier;
-
-		// CONTROL POINTS
-		Vector3 p0 = bezier.ControlPoint0.transform.position;
-		Vector3 p1 = bezier.ControlPoint1.transform.position;
-		Vector3 p2 = bezier.ControlPoint2.transform.position;
-		Vector3 p3 = bezier.ControlPoint3.transform.position;
-
-		List<Vector3> controlPoints = new List<Vector3>() { p0, p1, p2, p3 };
-
-		float resolucionBezier = 0.02f;
-
-		bezier._points = bezier.CreateBezierPoints(controlPoints, resolucionBezier);
-
-		// display an orange disc where the object is
-		Handles.color = Color.yellow;
-		for (int i = 0; i < bezier._points.Count - 1; i++)
+		if (cp != null)
 		{
-			Handles.DrawLine(bezier._points[i], bezier._points[i+1], 5.0f);
+			Bezier bezier = cp.bezier;
+
+			if (_lastPosition != cp.transform.position)
+				bezier.UpdateControlPoints();
+
+			// BEZIER:
+			Handles.color = Color.yellow;
+
+			float t = 0;
+			float resolution = 0.1f;
+			while (t <= 1)
+			{
+				Handles.DrawLine(bezier.GetBezierPointT(t), bezier.GetBezierPointT(t += resolution), 5.0f);
+			}
+
+			// Unity Bezier:
+
+			//Handles.DrawBezier(
+			//	bezier.ControlPoint0.transform.position,
+			//	bezier.ControlPoint3.transform.position,
+			//	bezier.ControlPoint1.transform.position,
+			//	bezier.ControlPoint2.transform.position,
+			//	Color.blue, Texture2D.whiteTexture, 5.0f
+			//	);
+
+			// LABEL
+			GUI.color = Color.yellow;
+			Handles.Label(bezier.GetBezierPointT(0.5f) + Vector3.up * 20, "Curva de Bezier");
+
+			_lastPosition = cp.transform.position;
 		}
-
-		//Handles.DrawBezier(
-		//	bezier.ControlPoint0.transform.position,
-		//	bezier.ControlPoint3.transform.position,
-		//	bezier.ControlPoint1.transform.position,
-		//	bezier.ControlPoint2.transform.position,
-		//	Color.blue, Texture2D.whiteTexture, 5.0f
-		//	);
-
-		// display object "value" in scene
-		GUI.color = Color.yellow;
-		Handles.Label(bezier._points[bezier._points.Count / 2] + Vector3.up, "Curva de Bezier");
 	}
+
 }
