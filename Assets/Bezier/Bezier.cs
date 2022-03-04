95645674f910259f00a073da272ec55732b60a81
David Martinez Alcantara <martinezalcantara29@gmail.com>
2022-03-04 23:02:17 +0100

    bezier pulida

--- Assets/Bezier/Bezier.cs
+++ Assets/Bezier/Bezier.cs
@@ -8,13 +8,10 @@ public class Bezier : MonoBehaviour
 {
 	public LineRenderer lineRenderer;
 	public bool renderLine = true;
+	public bool renderControlPoints = true;
 
-	public ControlPoint ControlPoint0;
-	public ControlPoint ControlPoint1;
-	public ControlPoint ControlPoint2;
-	public ControlPoint ControlPoint3;
-
-	public List<Vector3> controlPoints;
+	public ControlPoint[] constrolPoints;
+	public List<Vector3> cpPositions;
 	public List<Vector3> points = new List<Vector3>();
 
 	public decimal BezierResolution = 0.01m;
@@ -22,7 +19,7 @@ public class Bezier : MonoBehaviour
 	private float length = 0;
 
 	// Start is called before the first frame update
-	void Start()
+	void Awake()
 	{
 		lineRenderer = GetComponent<LineRenderer>();
 		
@@ -35,7 +32,8 @@ public class Bezier : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		lineRenderer.enabled = renderLine;
+		// line Renderer activado & esta en PlayMode
+		lineRenderer.enabled = renderLine && Application.isPlaying;
 	}
 
 	// Almacen/Tabla con los puntos de la curva (Vec3) segun la t porque se llama a esta funcion MUCHO
@@ -44,25 +42,25 @@ public class Bezier : MonoBehaviour
 	public Vector3 GetBezierPointT(decimal t)
 	{
 		if (t <= 0)
-			return controlPoints[0];
+			return cpPositions[0];
 		if (t >= 1)
-			return controlPoints[controlPoints.Count - 1];
+			return cpPositions[cpPositions.Count - 1];
 
 		if (tablaPuntosT.ContainsKey(t))
 			return tablaPuntosT[t];
 
 		// Grado de la curva de Bezier
-		int n = controlPoints.Count - 1;
+		int n = cpPositions.Count - 1;
 
 		// Hay que calcular la Sumatoria de la "Influencia" de cada Punto de Control en t
 		Vector3 p = Vector3.zero;
 
-		for (int i = 0; i < controlPoints.Count; ++i)
+		for (int i = 0; i < cpPositions.Count; ++i)
 		{
 			float comb = Combinatoria(n, i);
 
 			// GRADO n ==> p(t) = SUM[i=0,n] ( Pi * comb(n,i) * (1-t)^(n-i) * t^i )
-			p += (controlPoints[i] * (comb * Mathf.Pow((float)(1 - t), n - i) * Mathf.Pow((float)t, i)));
+			p += (cpPositions[i] * (comb * Mathf.Pow((float)(1 - t), n - i) * Mathf.Pow((float)t, i)));
 		}
 
 		tablaPuntosT[t] = p;
@@ -175,49 +173,6 @@ public class Bezier : MonoBehaviour
 		}
 
 		return GetT(s, iteraciones, GetLengthAcumuladaT(t, 0.001m), t, intervaloT);
-
-		/*
-		for (t = 0; t < 1; t += intervaloT)
-		{
-			// Espacio de cada intervalo chikito
-			espacioAcumulado += GetLengthIncrementoT(t, intervaloT);
-
-			// Continua aumentando hasta que supere el espacio que necesita
-			if (espacioAcumulado >= s)
-				break;
-		}
-
-		// Caso trivial: s esta al final de la curva
-		if (t >= 1) return t;
-
-		// Si esta en el margen de error, es ese el punto
-		if (Math.Abs(espacioAcumulado - s) < margenError)
-			return t;
-
-		// Por si acaso dejamos un maximo de intentos a que acote
-		int maxIteraciones = 20;
-		int iteraciones = 0;
-
-		// Acotamos arriba y abajo hasta que no se supere el margen de error
-		while (Math.Abs(espacioAcumulado - s) > margenError && iteraciones < maxIteraciones)
-		{
-			// Acoto el intervalo a la mitad
-			intervaloT /= 2;
-			if (espacioAcumulado - s > margenError) // Disminuyo la mitad si es positivo
-			{
-				t -= intervaloT;
-				espacioAcumulado -= GetLengthIncrementoT(t, intervaloT);
-			}
-			else // Aumento la mitad
-			{
-				t += intervaloT;
-				espacioAcumulado += GetLengthIncrementoT(t - intervaloT, intervaloT);
-			}
-
-			iteraciones++;
-		}
-		return t;
-		*/
 	}
 
 	public void UpdateLineRenderer()
@@ -230,12 +185,12 @@ public class Bezier : MonoBehaviour
 	// Actualiza los puntos de control cuando se muevan ingame
 	public void UpdateControlPoints()
 	{
-		controlPoints = new List<Vector3>()
+		cpPositions = new List<Vector3>()
 		{
-			ControlPoint0.transform.position,
-			ControlPoint1.transform.position,
-			ControlPoint2.transform.position,
-			ControlPoint3.transform.position
+			constrolPoints[0].transform.position,
+			constrolPoints[1].transform.position,
+			constrolPoints[2].transform.position,
+			constrolPoints[3].transform.position
 		};
 
 		UpdateBezierPoints();
@@ -248,8 +203,8 @@ public class Bezier : MonoBehaviour
 		tablaEspacioAcumulado.Clear();
 		points.Clear();
 
-		Vector3 inicio = controlPoints[0];
-		Vector3 final = controlPoints[controlPoints.Count - 1];
+		Vector3 inicio = cpPositions[0];
+		Vector3 final = cpPositions[cpPositions.Count - 1];
 
 		points.Add(inicio);
 
@@ -298,53 +253,20 @@ public class Bezier : MonoBehaviour
 		_combCache.Add(key, comb / factorial);
 		return _combCache[key];
 	}
-}
 
-
-/* UI con Handles que dibuja un Gizmo de la linea al seleccionar el Objeto con el Script
-
-// A tiny custom editor for ExampleScript component
-[CustomEditor(typeof(Bezier))]
-public class ShowCurveInEditor : Editor
-{
-	// Custom in-scene UI for when ExampleScript
-	// component is selected.
-	public void OnSceneGUI()
+	// =========================== GIZMO ===========================
+	private void OnDrawGizmos()
 	{
-		Bezier bezier = target as Bezier;
-		Transform tr = bezier.transform;
-		Vector3 pos = tr.position;
-
-		// CONTROL POINTS
-		Vector3 p0 = bezier.ControlPoint0.transform.position;
-		Vector3 p1 = bezier.ControlPoint1.transform.position;
-		Vector3 p2 = bezier.ControlPoint2.transform.position;
-		Vector3 p3 = bezier.ControlPoint3.transform.position;
-
-		List<Vector3> controlPoints = new List<Vector3>() { p0, p1, p2, p3 };
-
-		float resolucionBezier = 0.02f;
-
-		bezier._points = bezier.CreateBezierPoints(controlPoints, resolucionBezier);
-
-		// display an orange disc where the object is
-		Handles.color = Color.yellow;
-		for (int i = 0; i < bezier._points.Count - 1; i++)
+		// Curve
+		Gizmos.color = Color.yellow;
+		for (int i = 0; i < points.Count - 1; i++)
 		{
-			Handles.DrawLine(bezier._points[i], bezier._points[i+1], 5.0f);
+			Gizmos.DrawLine(points[i], points[i+1]);
 		}
 
-		//Handles.DrawBezier(
-		//	bezier.ControlPoint0.transform.position,
-		//	bezier.ControlPoint3.transform.position,
-		//	bezier.ControlPoint1.transform.position,
-		//	bezier.ControlPoint2.transform.position,
-		//	Color.blue, Texture2D.whiteTexture, 5.0f
-		//	);
-
-		// display object "value" in scene
-		GUI.color = Color.yellow;
-		Handles.Label(bezier._points[bezier._points.Count / 2] + Vector3.up, "Curva de Bezier");
+		// Ray Control Points
+		Gizmos.color = Color.white;
+		Gizmos.DrawLine(cpPositions[0], cpPositions[1]);
+		Gizmos.DrawLine(cpPositions[2], cpPositions[3]);
 	}
 }
-*/
--- Assets/Bezier/ControlPoint.cs
+++ Assets/Bezier/ControlPoint.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,60 +8,28 @@ using UnityEngine.UIElements;
 using Cursor = UnityEngine.Cursor;
 
 [ExecuteAlways]
-public class ControlPoint : DragObject
+public class ControlPoint : MonoBehaviour
 {
-	public Bezier Bezier;
+	void Awake()
+	{
+	}
+
+	void Update()
+	{
+		// Si se mueve un punto de control actualizamos Bezier
+		Bezier bezier = GetComponentInParent<Bezier>();
+		if (transform.hasChanged && bezier)
+		{
+			bezier.UpdateControlPoints();
+			bezier.UpdateLineRenderer();
+		}
+		if (!bezier)
+			print("No hay Bezier asignada a " + this.ToString());
+
+		enabled = bezier.renderControlPoints;
+	}
 
 	void OnMouseDrag()
 	{
-		Bezier.UpdateControlPoints();
-		Bezier.UpdateLineRenderer();
 	}
-}
-// Editor personalizado para Bezier
-[CustomEditor(typeof(ControlPoint))]
-public class ControlPointEditor : Editor
-{
-	// Guarda la ultima posicion del punto de control, si cambia ya se que tengo que actualizar la curva
-	private Vector3 _lastPosition;
-
-	// Cuando el Punto de Control esta seleccionado
-	public void OnSceneGUI()
-	{
-		ControlPoint cp = target as ControlPoint;
-		if (cp != null)
-		{
-			Bezier bezier = cp.Bezier;
-
-			if (_lastPosition != cp.transform.position)
-				bezier.UpdateControlPoints();
-
-			// BEZIER:
-			Handles.color = Color.yellow;
-
-			decimal t = 0;
-			decimal resolution = 0.05m;
-			while (t <= 1)
-			{
-				Handles.DrawLine(bezier.GetBezierPointT(t), bezier.GetBezierPointT(t += resolution), 5.0f);
-			}
-
-			// Unity Bezier:
-
-			//Handles.DrawBezier(
-			//	bezier.ControlPoint0.transform.position,
-			//	bezier.ControlPoint3.transform.position,
-			//	bezier.ControlPoint1.transform.position,
-			//	bezier.ControlPoint2.transform.position,
-			//	Color.blue, Texture2D.whiteTexture, 5.0f
-			//	);
-
-			// LABEL
-			GUI.color = Color.yellow;
-			Handles.Label(bezier.GetBezierPointT(0.5m) + Vector3.up * 20, "Curva de Bezier");
-
-			_lastPosition = cp.transform.position;
-		}
-	}
-
 }
