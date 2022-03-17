de1ed98a3f103a777b648a40bdb96bf7f8b5ec1c
David Martinez Alcantara <martinezalcantara29@gmail.com>
2022-03-17 17:28:46 +0100

    videojuegos

--- Assets/Scripts/Enemies/EnemyShoot.cs
+++ Assets/Scripts/Enemies/EnemyShoot.cs
@@ -7,12 +7,16 @@ public class EnemyShoot : MonoBehaviour
 	public GameObject bulletPrefab;
 	public float shootFrecuency = 1.5f;
 	public float shootForce = 10;
+	public float rotationSpeed = 0.5f;
 
 	private GameObject target;
 	[SerializeField] private List<GameObject> bulletsPool;
 	private int numBullets;
 
 	private Vector3 BulletInitPosition;
+
+	private bool rotateTowardsPlayer = false;
+
 
 	// Start is called before the first frame update
 	void Awake()
@@ -22,7 +26,7 @@ public class EnemyShoot : MonoBehaviour
 		BulletInitPosition = transform.position;
 		BulletInitPosition += Vector3.up * 1.5f;
 
-		                      numBullets = (int)Mathf.Round(shootFrecuency * 10);
+							  numBullets = (int)Mathf.Round(shootFrecuency * 10);
 		for (int i = 0; i < numBullets; i++)
 		{
 			GameObject bullet = Instantiate(bulletPrefab, transform);
@@ -35,17 +39,24 @@ public class EnemyShoot : MonoBehaviour
 
 	void Start()
 	{
-		StartCoroutine(shootRoutine());
-	}
-	
+		StartCoroutine(ShootRoutine());
+		StartCoroutine(CheckIfPlayerInFront());
 
-	IEnumerator shootRoutine()
+		if (rotateTowardsPlayer)
+		{
+
+			Vector3 targetDir = (target.transform.position - transform.position).normalized;
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetDir), rotationSpeed * Time.deltaTime);
+		}
+	}
+
+
+	IEnumerator ShootRoutine()
 	{
-		while(true)
+		while (true)
 		{
 			yield return new WaitForSeconds(1 / shootFrecuency + Random.value);
 
-			print("Shoot");
 
 			// Mover de la Pool a la lista de activos
 			GameObject bullet = bulletsPool[numBullets - 1];
@@ -58,11 +69,28 @@ public class EnemyShoot : MonoBehaviour
 			if (!rb)
 				print(ToString() + " No tiene RigidBody!!!!");
 
-			Vector3 shootDirection = target.gameObject.transform.position - transform.position;
+			Vector3 shootDirection = (target.gameObject.transform.position - transform.position).normalized;
 
 			rb.AddForce(shootDirection * shootForce);
 			bullet.transform.rotation = Quaternion.LookRotation(shootDirection);
 			bullet.transform.position = BulletInitPosition;
+		}
+	}
+
+	IEnumerator CheckIfPlayerInFront()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(Mathf.Abs(Random.value - 0.5f));
+
+			print("Rotate");
+
+			// Mover de la Pool a la lista de activos
+			Vector3 targetDir = (target.transform.position - transform.position).normalized;
+			Vector3 enemyForward = transform.forward.normalized;
+
+			// Si el DOT es menos de 1 es que no tienen similar direccion
+			rotateTowardsPlayer = Vector3.Dot(targetDir, enemyForward) < 0.9;
 		}
 	}
 
