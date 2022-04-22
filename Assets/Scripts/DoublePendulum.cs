842635a87532a579f7527cf70ce30b28cce9fea7
David Martinez Alcantara <martinezalcantara29@gmail.com>
2022-04-22 16:46:58 +0200

    PENDULOS

--- Assets/Scripts/DoublePendulum.cs
+++ Assets/Scripts/DoublePendulum.cs
@@ -2,34 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoublePendulum : MonoBehaviour
 {
 	public Pendulum p1;
 	public Pendulum p2;
 
-	public float speed = .05f;
+	public float initAngle1 = 100;
+	public float initAngle2 = -30;
+
+	public float ropeLength1 = 5;
+	public float ropeLength2 = 5;
+
+	public float animationSpeed = 1;
 
 	public Color color;
 
+	public float minAngularVelocity = -10;
+	public float maxAngularVelocity = 10;
+
 	void Start()
 	{
-		//ParticleSystem ps = p2.GetComponent<ParticleSystem>();
-		//ParticleSystem.MainModule mainModule = ps.main;
-		//mainModule.startColor = color;
+		// Cambiar el Color del trazo
+		ParticleSystem ps = p2.GetComponent<ParticleSystem>();
+		ParticleSystem.MainModule psMain = ps.main;
+		psMain.startColor = new ParticleSystem.MinMaxGradient(color);
+
+		ParticleSystem.TrailModule psTrails = ps.trails;
+		psTrails.colorOverLifetime = color;
+
+		p1.ropeLength = ropeLength1;
+		p2.ropeLength = ropeLength2;
+
+		p1.angle = initAngle1;
+		p2.angle = initAngle2;
+
+		Vector3 pos = transform.position;
+		p1.transform.localPosition = new Vector3(Mathf.Sin(Mathf.Deg2Rad * initAngle1) * p1.ropeLength, Mathf.Cos(Mathf.Deg2Rad * initAngle1) * p1.ropeLength, 0);
+		p2.transform.localPosition = new Vector3(Mathf.Sin(Mathf.Deg2Rad * initAngle2) * p2.ropeLength, Mathf.Cos(Mathf.Deg2Rad * initAngle2) * p2.ropeLength, 0);
 	}
 	
-	void Update()
+	void FixedUpdate()
 	{
-		float g = Physics.gravity.magnitude;
+		float g = Physics.gravity.magnitude * Time.fixedDeltaTime * Time.fixedDeltaTime;
 		float m1 = p1.GetComponent<Rigidbody>().mass;
 		float m2 = p2.GetComponent<Rigidbody>().mass;
 
-		float l1 = p1.ropeDir.magnitude;
-		float l2 = p2.ropeDir.magnitude;
+		float l1 = p1.ropeLength;
+		float l2 = p2.ropeLength;
 
-		float a1 = p1.angle;
-		float a2 = p2.angle;
+		float a1 = Mathf.Deg2Rad * p1.angle;
+		float a2 = Mathf.Deg2Rad * p2.angle;
 		float v1 = p1.angularVel;
 		float v2 = p2.angularVel;
 
@@ -43,7 +67,21 @@ public class DoublePendulum : MonoBehaviour
 							+ v2 * v2 * l2 * m2 * Mathf.Cos(a1 - a2))
 							/ (l2 * (2 * m1 + m2 - m2 * Mathf.Cos(2 * a1 - 2 * a2)));
 
-		p1.angularForce *= Time.deltaTime * speed;
-		p2.angularForce *= Time.deltaTime * speed;
+		p1.angularForce *= Time.fixedDeltaTime * animationSpeed;
+		p2.angularForce *= Time.fixedDeltaTime * animationSpeed;
+
+		p1.angularForce = Mathf.Clamp(p1.angularForce, -1, 1);
+		p2.angularForce = Mathf.Clamp(p2.angularForce, -1, 1);
+
+
+		p1.angularVel += p1.angularForce;
+		p2.angularVel += p2.angularForce;
+
+		// Limit Velocity:
+		p1.angularVel = Mathf.Clamp(p1.angularVel, minAngularVelocity, maxAngularVelocity);
+		p2.angularVel = Mathf.Clamp(p2.angularVel, minAngularVelocity, maxAngularVelocity);
+
+		p1.angle += p1.angularVel;
+		p2.angle += p2.angularVel;
 	}
 }
--- Assets/Scripts/Pendulum.cs
+++ Assets/Scripts/Pendulum.cs
@@ -5,29 +5,30 @@ public class Pendulum : MonoBehaviour
 {
 	public float speed = .01f;
 	
-	public float angle;
-	public Vector3 ropeDir;
+	public float angle = 60;
+	public float ropeLength = 1;
 
 	public float angularVel = 0;
-	public float angularForce = 0.1f;
+	public float angularForce = 1f;
 
-	void Start()
+	void Awake()
 	{
-		ropeDir = transform.position - transform.parent.position;
+		Vector3 ropeDir = transform.position - transform.parent.position;
+		ropeLength = ropeDir.magnitude;
 		angle = Mathf.Acos(Vector3.Dot(ropeDir.normalized, new Vector3(0, ropeDir.y, 0).normalized));
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
-		ropeDir = transform.position - transform.parent.position;
 		
 		//angularForce = getSinglePendulumForce();
 
-		angularVel += angularForce;
-		angle += Mathf.Deg2Rad * angularVel;
+		//angularVel += angularForce;
+		//angle += angularVel;
 
-		transform.localPosition = new Vector3(Mathf.Sin(angle), -Mathf.Cos(angle), 0);
-		transform.localPosition *= ropeDir.magnitude;
+
+		transform.localPosition = new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), -Mathf.Cos(Mathf.Deg2Rad * angle), 0);
+		transform.localPosition *= ropeLength;
 	}
 
 	float getSinglePendulumForce()
@@ -37,7 +38,7 @@ public class Pendulum : MonoBehaviour
 		float m = rb.mass;
 		float g = Physics.gravity.magnitude;
 
-		return -m * g * Mathf.Sin(angle) * Time.deltaTime * speed;
+		return -m * g * Mathf.Sin(Mathf.Deg2Rad * angle) * Time.fixedDeltaTime * speed;
 	}
 
 	void OnDrawGizmos()
